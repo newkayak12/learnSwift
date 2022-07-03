576127c101af366995376839f489f0d424e9d42b
newkayak12 <newkayak12@gmail.com>
2022-07-03 18:26:52 +0900

    learn Swift

---
+++ learnSwift/00.abountSwift/01.swiftConsole.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var str = "HelloSwift, HelloPlayGround";
+print(str);
+var j: Int = 0;
+
+for i in 0...3{
+    j+=i;
+}
+/**
+    위와 같이 값이 변경되거나 반복 실행 등의 작업을 하게 되면
+ */
---
+++ learnSwift/00.aboutSwift/00.about.swift
@@ -0,0 +1,9 @@
+//
+//  00.about.swift
+//  learnSwift
+//
+//  Created by Sang Hyeon kim on 2022/07/03.
+//
+
+import Foundation
+print("hello")
