58a57b4cabd7696311a47739bf8473bc48d15e00
newkayak12 <newkayak12@gmail.com>
2022-07-26 22:15:35 +0900

    swift

--- learnSwift/02.oop/04.accessControl.playground/Contents.swift
+++ learnSwift/02.oop/04.accessControl.playground/Contents.swift
@@ -130,5 +130,184 @@ private func privateFunction() {}
 
 /**
     4. 접근제어 구현 참고사항
- 모든 타입에 적용되는 접근 수준의 규칙은 '상위 요소보다 하위 요소가 더 높은 접근수준을 가질수 없다.'이다.
+ 모든 타입에 적용되는 접근 수준의 규칙은 '상위 요소보다 하위 요소가 더 높은 접근수준을 가질수 없다.'이다. 이 말은 비공개 접근수준으로 정의한 구조체 내부의 프로퍼티로 내부 수준이나 공개 수준을 갖는 프로퍼티를 정의할 수 없다. 또, 함수의 매개변수로 특정 접근수준이 부여된 타입이 전달되거나 반환된다면, 그 타입의 접근수준보다 함수의 접근 수준이 높게 설정될 수 없다.
+ 
+    //잘못된 접근 수준 부여 예시
+ 
+     private class AClass{
+        //공개 접근 수준을 부여해도 AClass 접근 수준이 비공개 접근수준이므로
+        // 이 메소드의 접근 수준도 비공개로 취급된다.
+ 
+        public func someMethod(){  -> (X) 안된다.
+            // ...
+        }
+     }
+ 
+    //AClass의 접근 수준이 비공개 접근수준이므로
+    //공개 접근 수준의 함수의 매개변수나 반환 값 타입으로 사용할 수 없다.
+ 
+    public func someFunction(a: AClass) -> AClass { //오류 발생
+        return a;
+    }
+ 
+ 함수뿐만 아니라 튜플의 내부 요소 타입 또한 튜플의 접근수준보다 같거나 높아야한다.
+ 
+    internal class InternalClass {  } //내부 접근 수준 클래스
+    private struct PrivateStruct {  } // 비공개 접근 수준 구조체
+ 
+ //요소로 사용되는 InternalClass와 PrivateStruct의 접근 수준이
+ //publicTuple보다 낮기 때문에 사용할 수 없다.
+ public var publicTuple: (first: InternalClass, second: PrivateStruct) = (InternalClass(), PrivateStruct())
+ 
+ //요소로 사용되는 InternalClass와 PrivateStruct의 접근 수준이 privateTuple과 같거나 높기 때문에 사용할 수 있다.
+ private var privateTuple: (first: InternalClass, second: PrivateStruct) = (InternalClass(), PrivateStruct())
  */
+
+
+//let aInstance: AClass = AClass();
+//aInstance.internalMethod();  //같은 모듈일 경우 호출 가능
+//aInstance.filePrivateMethod(); //다른 파일이므로 호출 불가
+//aInstance.internalProperty = 1; //같은 모듈이므로 접근 가능
+//aInstance.filePrivateProperty = 1; //다른 파일이므로 접근 불가
+
+/**
+    위와 같이 접근수준에 따라 접근이 불가능한 경우가 생긴다. 그렇기 때문에 프레임워크를 만들 떄는 다른 모듈에서 특정 기능에 접근할 수 있도록 API로 사용할 기능을 공개 접근 수준(public)으로 지정해줘야한다. 그 이외의 요소는 내부 접근 수준 또는 비공개 접근 수준으로 적절히 설정하면된다.
+ 
+    열거형의 접근 수준을 구현할 때 열거형 내부에 각 case 별로 따로 접근수준을 부여할 수는 없다. 각 case의 접근 수준은 열거형 자체의 접근수준을 따른다. 또한 열거형의 원시 값 타입으로 열거형의 접근수준보다 낮은 접근수준의 타입이 올 수는 없다. 연관 값의 타입 또한 마찬가지이다.
+ 
+    private typealias PointValue = Int
+    
+    //오류 - PointValue가 Point보다 접근 수준이 낮기 때문에 원시 값으로 사용할 수 없다.
+    enum Point: PointValue {
+        case x, y
+    }
+ 
+ 
+    5. private와 fileprivate
+ 같은 파일 내부에서 private 접근수준과 fileprivate 접근 수준은 사용할 때 분명한 차이가 있다. fileprivate 접근 수준으로 지정한 요소는 같은 파일 어떤 코드에서도 접근할 수 있다.
+ 
+ 반면에 private 접근수준으로 지정한 요소는 같은 파일 내부에 다른 타입의 코드가 있더라도 접근이 불가능하다. 그러나 자신 을 확장하는 익스텐션 코드가 같은 파일에 존재하는 경우에 접근할 수 있다.
+ 
+ */
+public struct SomeType{
+    private var privateVariable = 0;
+    fileprivate var fileprivateVariable = 0;
+}
+
+//같은 타입의 익스텐션에서는 private 요소에 접근 가능
+extension SomeType{
+    public func publicMethod(){
+        print("\(self.privateVariable), \(self.fileprivateVariable)")
+    }
+    private func privateMethod(){
+        print("\(self.privateVariable), \(self.fileprivateVariable)")
+    }
+    fileprivate func filePrivateMethod(){
+        print("\(self.privateVariable), \(self.fileprivateVariable)")
+    }
+}
+let someType = SomeType()
+someType.publicMethod()
+someType.filePrivateMethod()
+//someType.privateMethod()
+
+struct AnotherType{
+    var someInstance: SomeType = SomeType()
+    mutating func someMethod(){
+//        public 접근 수준에서는 어디서든 접근 가능
+        self.someInstance.publicMethod()
+//         같은 파일에 속해 있는 코드이므로 fileprivate 접근 수준 요소에 접근 가능
+        self.someInstance.fileprivateVariable = 100
+        self.someInstance.filePrivateMethod()
+        
+//        다른 타입 내부의 코드이므로 private 요소에 접근이 불가능하다.
+//        self.someInstance.filePrivateMethod() = 100
+//        self.someInstance.filePrivateMethod()
+    }
+}
+
+var anotherInstance = AnotherType()
+anotherInstance.someMethod()
+
+/**
+        6. 읽기 전용 구현
+ 구조체 또는 클래스 사용하여 저장 프로퍼티를 구현할 떄는 허용된 접근 수준에서 프로퍼티 값을 가져갈 수 있다 그러면 값을 변경할 수 없도록 구현하려면 어떻게 해야한다. 또, 서브스크립트도 읽기만 가능하려면 어떻게 해야할까?
+ 
+ 그럴 때는 설정자만 낮은 접근 수준을 갖도록 제한할 수 있다. 설정자 접근 수준 제한은 프로퍼티, 서브스크립트, 변수 등에 적용될 수 있으며, 해당 요소의 접근 수준보다 같거나 낮은 수준으로 제한해줘야한다.
+ */
+public struct SomeType2{
+    //비공개 접근 수준 저장 프로퍼티
+    private var count: Int = 0
+    //공개 접근 수준 저장 프로퍼티
+    public var publicStoredProperty: Int = 0;
+    
+    //공개 접근 수준 저장 프로퍼티, 설정자는 비공개
+    public private(set) var publicGetOnlyStoredProperty: Int = 0
+    
+    //내부 접근 수준 저장 프로퍼티
+    internal var internalComputedProperty: Int{
+        get{
+            self.count
+        }
+        set{
+            self.count += 1
+        }
+    }
+    
+    //내부 접근수준 저장 프로퍼티 설정자는 비공개
+    internal private(set) var internalgetOnlyComputedProperty: Int{
+        get{
+            self.count
+        }
+        set{
+            self.count += 1
+        }
+    }
+    
+    //공개 수준 서브스크립트
+    public subscript() -> Int {
+        get{
+            self.count
+        }
+        set{
+            count += 1
+        }
+    }
+    
+    //공개 접근 수준 서브스크립트, 설정자는 내부 접근 수준
+    public internal(set) subscript(some: Int) -> Int {
+        get{
+            self.count
+        }
+        set{
+            self.count += 1
+        }
+    }
+}
+
+var someInstance: SomeType2 = SomeType2()
+//외부에서 접근, 생성 모두 가능
+print(someInstance.publicStoredProperty)
+someInstance.publicStoredProperty = 100
+
+//외부에서 접근자만 사용 가능
+print(someInstance.publicGetOnlyStoredProperty)
+//someInstance.publicGetOnlyStoredProperty = 100 //에러
+
+//외부에서 접근자, 설정자 모두 가능
+print(someInstance.internalComputedProperty)
+someInstance.internalComputedProperty = 99
+
+//외부에서 접근자만 사용 가능
+print(someInstance.internalgetOnlyComputedProperty)
+//someInstance.internalgetOnlyComputedProperty = 1 //에러
+
+
+
+//외부에서 접근, 설정 모두 가능
+print(someInstance[])
+someInstance[] = 100
+
+//외부에서 접근자만, 같은 모듈 내에서는 설정자도 사용 가능
+print(someInstance[0])
+someInstance[0] = 100
---
+++ learnSwift/02.oop/04.accessControl.playground/Sources/ExampleAClass.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+public class AClass{
+    
+    public init(){  }
+    func internalMethod(){print("INTERNALMETHOD")}
+    fileprivate func filePrivateMethod() {print("filePrivateMethod")}
+    var internalProperty = 0
+    fileprivate var filePrivateProperty = 0
+}
+
